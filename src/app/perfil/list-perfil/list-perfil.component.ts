cfe881af920e31a487ade278771ee966209eeb29
Thiago Aguiar <thiagoferreiradeaguiar@gmail.com>
2019-12-24 23:54:36 -0300

    label da tabela.

--- src/app/perfil/list-perfil/list-perfil.component.ts
+++ src/app/perfil/list-perfil/list-perfil.component.ts
@@ -19,13 +19,50 @@ export class ListPerfilComponent implements OnInit {
   titlePage: string;
   sigla: string;
   descricao: string;
+  pageForm: string;
   listaPerfilUsuarioLTO: PerfilUsuarioLTO[] = [];
+  totalResgistros: number;
+  firstPage: number;
+  endPage: number;
+  qtdRows: number;
 
   ngOnInit() {
-    this.titlePage = "Perfil";    
+    this.titlePage = "Perfil";
+    this.pageForm = "/form-perfil";
+    this.qtdRows = 10;
     this.perfilService.listarTodos().subscribe((response: ResponseEntity) => {
       this.listaPerfilUsuarioLTO = response.data;
+      this.totalResgistros = this.listaPerfilUsuarioLTO.length;
+      this.showLabelPaginate(0);
     });
+  }
+
+  public pesquisarPerfilUsuario() {
+    this.perfilService.pesquisarPerfilUsuario(this.sigla, this.descricao).subscribe((response: ResponseEntity) => {
+      this.listaPerfilUsuarioLTO = response.data;
+      this.totalResgistros = this.listaPerfilUsuarioLTO.length;
+      this.showLabelPaginate(0);
+    });
+  }
+
+  public exibirDadosPerfil(event: any) {
+    this.router.navigate([this.pageForm + "/" + event.data.idPerfilUsuario]);
+  }
+
+  public paginate(event: any) {
+    this.showLabelPaginate(event.first);
+  }
+
+  private showLabelPaginate(page: number) {
+    var paginaAtual = page + 1;
+    this.firstPage = (this.qtdRows * page) + 1;
+
+    var quociente = (this.totalResgistros / this.qtdRows);
+    if (parseInt(quociente.toString()) > paginaAtual) {
+      this.endPage = this.qtdRows * paginaAtual;
+    } else {
+      this.endPage = this.totalResgistros;
+    }
   }
 
 }
